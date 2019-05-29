323520d9071e04c8c673988a62c2a3407b7f666a
Melissa Real <melissa.real@hotmail.com>
2019-05-29 15:29:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 $(document).ready(function (){
-
-
-      $.get('results', function(response) {
-          // Here we are getting the element on the page with the id of sentences and
-          // inserting the response
-          $('#results').html(response);
-
-
+  var url =
+    'https://api.github.com/repos/';
+ 
+$.get(url).function(response) {
+        // Here we are getting the element on the page with the id of sentences and
+        // inserting the response
+        $('#results').html(response);
+    });
 });
