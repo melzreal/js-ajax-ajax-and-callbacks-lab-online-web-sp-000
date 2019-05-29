26155bd83d2a0523c3da642519308f6827744db8
Melissa Real <melissa.real@hotmail.com>
2019-05-29 15:30:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function (){
   var url =
-    'https://api.github.com/repos/';
+    ' https://api.github.com/repos/jquery/jquery/commits?author=jeresig';
  
 $.get(url).function(response) {
         // Here we are getting the element on the page with the id of sentences and
