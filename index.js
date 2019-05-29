d3d1a40b89fdb9e023c67c8e7f6e9def7c3ad516
Melissa Real <melissa.real@hotmail.com>
2019-05-29 15:25:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 $(document).ready(function (){
 
-  
+
       $.get('results', function(response) {
           // Here we are getting the element on the page with the id of sentences and
           // inserting the response
           $('#sentences').html(response);
 
-  
+
 });
