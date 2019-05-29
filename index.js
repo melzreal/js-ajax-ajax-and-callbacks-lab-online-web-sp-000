d8fb093d43633d3a6776a7ad6a3e522d72397e7e
Melissa Real <melissa.real@hotmail.com>
2019-05-29 15:32:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-$(document).ready(function (){
-  var url ='https://api.github.com/repos/jquery/jquery/commits?author=jeresig';
-
-$.get(url).function(response) {
-        // Here we are getting the element on the page with the id of sentences and
-        // inserting the response
-        $('#results').html(response);
-    });
+var url =
+    'https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078';
+ 
+$.get(url).done(function(data) {
+    console.log('Done');
+    console.log(data);
 });
