7bde6b1b2503475f8bb238adbe80ee160cd10b10
Melissa Real <melissa.real@hotmail.com>
2019-05-29 15:44:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 $(document).ready(function (){
-  var url =
-      'https://api.github.com/repos/jquery/jquery/commits?sha=8f447576c918e3004ea479c278c11677920dc41a';
-
-  $.get(url).done(function(data) {
-      console.log('Done');
-      console.log(data);
-      $('results').html(data);
-  });
 
 });
+
+
+function searchRepositories(){
+  
+}
